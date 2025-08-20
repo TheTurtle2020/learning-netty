f636a063074e7b9850e0fb93283783c7e394c49b
TheTurtle <almostsir2022@gmail.com>
2025-08-20 22:07:03 +0800

    修改暂停位置

--- src/test/java/cn/itcast/nio/c5/AioFileChannel.java
+++ src/test/java/cn/itcast/nio/c5/AioFileChannel.java
@@ -34,9 +34,9 @@ public class AioFileChannel {
                 }
             });
             log.debug("read end...");
+            System.in.read();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.in.read();
     }
 }
